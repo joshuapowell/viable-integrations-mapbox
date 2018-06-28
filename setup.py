b572bea136b86ebc562ab0c8214c71398cb23012
Joshua Powell <joshua@viable.io>
2018-06-28 17:19:21 -0400

    Added viable_mapbox package

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@
 from setuptools import setup
 
 setup(
-    name="viable-integrations-mapbox",
+    name="viable_mapbox",
     version="1.0.0",
     description="Mapbox integration for Viable Cloud API",
     url="https://github.com/viableindustries/viable-integrations-mapbox",
     author="Viable Industries, L.L.C.",
     license="MIT",
     keywords="python setuptools package installer pip mapbox viablecloudapi",
-    packages=["viable-integrations-mapbox"],
+    packages=["viable_mapbox"],
     include_package_data=True,
     install_requires=[
         "flask"
